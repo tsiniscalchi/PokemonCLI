31f920a66b6acd5f6af98cfc9bac09e0e7828986
Trevor Siniscalchi <Trevorsiniscalchi@yahoo.com>
2020-11-18 09:53:38 -0800

    changes

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-function main(args) {
-    // ...
+async function getPokemonTypes(pokemon) {
+    return await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(response => response.json())
+        .then(json => json.types);
 }
 
-main(process.argv);
+async function main(args) {
+    var lines = fs.readFileSync(args[2]).toString().replace("\r", "").split("\n");
+
+    lines.forEach(async function(pokemon) {
+        var types = await getPokemonTypes(pokemon);
+        var outputString = "";
+
+        var names = pokemon.split(" ");
+
+        names.forEach((item, index) => {
+            var name = item.split("");
+
+            name[0] = name[0].toUpperCase();
+            outputString += name.join("");
+
+            if (index != names.length - 1) outputString += " ";
+            else outputString += ": ";
+        });
+
+        types.forEach((item, index) => {
+            outputString += item.type.name;
+            if (index != types.length - 1) outputString += ", ";
+        });
+
+        console.log(outputString);
+    });
+}
